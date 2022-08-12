f5c9486214ca6535fda951aed520ae3eef8275b0
abdulmoizlogiciel <abdul.moiz@logicielservice.com>
2022-08-12 19:22:14 +0500

    Add project files.

---
+++ HashPassword/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace HashPassword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = args.ElementAtOrDefault(0);
+            string salt = args.ElementAtOrDefault(1);
+
+            if (password is null)
+            {
+                Console.WriteLine("Password is null.");
+                return;
+            }
+            if (salt is null)
+            {
+                Console.WriteLine("Salt is null.");
+                return;
+            }
+
+            GenerateHash(password, salt);
+        }
+
+        private static void GenerateHash(string password, string salt)
+        {
+            //string password = "qa";
+            //string salt = "6UOnMNpHE1M=";
+
+            string hash = SecurityHelper.HashPassword(password, salt);
+            Console.WriteLine(password);
+            Console.WriteLine(salt);
+            Console.WriteLine(hash);
+        }
+    }
+}
---
+++ HashPassword/SecurityHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace HashPassword
+{
+    public class SecurityHelper
+    {
+        private const int nSalt = 8;
+        private const int nHash = 32;
+
+        public static string GenerateSalt()
+        {
+            var saltBytes = new byte[nSalt];
+
+            using (var provider = new RNGCryptoServiceProvider())
+            {
+                provider.GetNonZeroBytes(saltBytes);
+            }
+
+            return Convert.ToBase64String(saltBytes);
+        }
+
+        public static string HashPassword(string password, string salt)
+        {
+            var saltBytes = Convert.FromBase64String(salt);
+
+            using var rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, saltBytes);
+            return Convert.ToBase64String(rfc2898DeriveBytes.GetBytes(nHash));
+        }
+    }
+}
